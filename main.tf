4fe52caa1f0f05868e511ede8f7fb8c685958dbe
delude88 <tobias.hegemann@googlemail.com>
2020-11-11 01:13:57 +0100

    Added main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+terraform {
+    backend "remote" {
+        # The name of your Terraform Cloud organization.
+        organization = "digital-stage"
+
+        # The name of the Terraform Cloud workspace to store Terraform state files in.
+        workspaces {
+            name = "dev"
+        }
+    }
+}
