44b6b96480e7b70aed098f756aacf2e5e40761fd
delude88 <tobias.hegemann@googlemail.com>
2020-11-11 01:58:41 +0100

    Fixed reference to ssh key

--- provider.tf
+++ provider.tf
@@ -14,6 +14,6 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-data "digitalocean_ssh_key" "terraform" {
-  name = "terraform"
+data "digitalocean_ssh_key" "DigitalStage" {
+  name = "DigitalStage"
 }
